4ab36eefe71bf22c90a771380ae0bc7e1004ebaf
Randy <phpgeweld@gmail.com>
2021-01-25 13:54:02 +0100

    Started with first area

--- src/areas/tutorial/locations/start.js
+++ src/areas/tutorial/locations/start.js
@@ -1,7 +1,7 @@
 module.exports = {
 
-    label: 'Start',
-    description: `Welcome to Tutorial island! This is where you start your adventure in runecord\n` +
+    label: 'Starter location',
+    description: `Welcome to Tutorial island! This is where you start your adventure in runecord\n\n` +
         `To travel to a other location you can use the \`%prefix%walk [location]\` command.\n` +
         `To see what locations are in your current area, you can use the \`%prefix%area\` command \n` +
         `To see more info about your current location, you can use the \`%prefix%location\` command`
