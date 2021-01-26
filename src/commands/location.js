4466d0ba07bb1a1ca841afaa9121406e23724550
Randy <phpgeweld@gmail.com>
2021-01-26 20:34:13 +0100

    Added resources to location command

--- src/commands/location.js
+++ src/commands/location.js
@@ -12,11 +12,22 @@ module.exports = {
         const fields = [];
 
         if(npcs && npcs.length) {
-            const npcField = {name: 'NPCs', value: ''};
+            const npcField = {name: 'NPCs', value: '', inline: true};
             for(let i in npcs) {
                 npcField.value += `- ${npcs[i].label}\n`;
             }
             fields.push(npcField);
+        }
+
+        if(areaData.locations[data.user.location].commands && Object.values(areaData.locations[data.user.location].commands).length) {
+            const resourcesField = {name: 'Resources', value: '', inline: true};
+            const commands = areaData.locations[data.user.location].commands;
+            for(let i in commands) {
+                for(let j in commands[i]) {
+                    resourcesField.value += `${valuesHelper.ucfirst(j)}\n`;
+                }
+            }
+            fields.push(resourcesField);
         }
         const embed = {
             title: areaData.locations[data.user.location].label,
