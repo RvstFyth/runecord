959343c44c9afcc692560baf956f63c136ed45ff
Randy <phpgeweld@gmail.com>
2021-01-26 19:37:05 +0100

    Changed how resources are defined for locations

--- src/areas/tutorial/locations/mines/mines.js
+++ src/areas/tutorial/locations/mines/mines.js
@@ -2,5 +2,23 @@ module.exports = {
 
     label: `Mines`,
     description: `Dit is een faketekst. Alles wat hier staat is slechts om een indruk te geven van het grafische effect van tekst op deze plek. Wat u hier leest is een voorbeeldtekst. Deze wordt later vervangen door de uiteindelijke tekst, die nu nog niet bekend is. De faketekst is dus een tekst die eigenlijk nergens over gaat. Het grappige is, dat mensen deze toch vaak lezen. Zelfs als men weet dat het om een faketekst gaat, lezen ze toch door.`,
-    commands: ['mine']
+    commands: {
+        mine: {
+            copper: {
+                xp: 17,
+                id: 3,
+                label: 'copper ore'
+            },
+            tin: {
+                xp: 17,
+                id: 3,
+                label: 'tin ore'
+            },
+            clay: {
+                xp: 17,
+                id: 3,
+                label: 'clay'
+            },
+        }
+    }
 };
--- src/commands/chop.js
+++ src/commands/chop.js
@@ -3,36 +3,29 @@ const skillsModel = require('../models/usersSkills');
 const inventoryModel = require('../models/usersInventory');
 const itemsModel = require('../models/items');
 
-const lootMapping = {
-    tutorial: {
-        forest: {
-            xp: 25,
-            item: 1
-        }
-    }
-};
-
 module.exports = {
 
 
     async run(msg, args, data)
     {
         const locationDetails = areasHelper.getLocation(data.user.area, data.user.location);
-        if(!locationDetails.commands || (locationDetails.commands && locationDetails.commands.indexOf('chop') < 0)) return msg.channel.send(`**${data.user.name}** there nothing to chop here?!?`);
+        if(!locationDetails.commands || (locationDetails.commands && !locationDetails.commands.chop)) return msg.channel.send(`**${data.user.name}** there nothing to chop here?!?`);
+
+        if(!args[0]) return msg.channel.send(`**${data.user.name}** what are you trying to chop?!?`);
+
+        if(!locationDetails.commands.chop[args[0]]) return msg.channel.send(`**${data.user.name}** you can't chop ${args[0]} here..`);
 
         const occupiedSlots = await inventoryModel.getOccupiedSlotCount(data.user.id);
         if(occupiedSlots >= 28) return msg.channel.send(`**${data.user.name}** your backpack is full..`);
 
         const skillRecord = await skillsModel.getFor(data.user.id, 'woodcutting');
-        const currentXP = parseInt(skillRecord.xp);
-        const level = 1;
 
-        const reward = lootMapping[data.user.area][data.user.location];
+        const reward = locationDetails.commands.chop[args[0]];
 
         await skillsModel.addXp(skillRecord.id, reward.xp);
-        await inventoryModel.add(data.user.id, reward.item, 1);
+        await inventoryModel.add(data.user.id, reward.id, 1);
 
-        const item = await itemsModel.get(reward.item);
+        const item = await itemsModel.get(reward.id);
         return msg.channel.send(`**${data.user.name}** chopped a tree and got a ${item.name} and ${reward.xp}xp!`);
     }
 };
