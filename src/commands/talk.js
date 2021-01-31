46201aa87feb358182fa13c52561ea709cca5d47
Randy <phpgeweld@gmail.com>
2021-01-31 12:56:16 +0100

    Send msg that initialized talk command

--- src/commands/talk.js
+++ src/commands/talk.js
@@ -12,7 +12,7 @@ module.exports = {
 
         const embed = {
             title: npc.label,
-            description: await npc.talk({userID: data.user.id})
+            description: await npc.talk({userID: data.user.id, msg: msg})
         };
 
         return msg.channel.send({embed});
