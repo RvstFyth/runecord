56bf7eb153f281de8548da2c6569aa6f990dd2f6
Randy <phpgeweld@gmail.com>
2021-02-06 12:22:16 +0100

    Fixed xp in chop command

--- src/commands/chop.js
+++ src/commands/chop.js
@@ -24,17 +24,18 @@ module.exports = {
 
         const reward = locationDetails.commands.chop[args[0]];
 
+        let xpGain = reward.xp;
         if(data.user.max_area === 'tutorial' && (parseInt(skillRecord.xp) + reward.xp > skillsHelper.xpForLevel(3))) {
             let diff = skillsHelper.xpForLevel(3) - parseInt(skillRecord.xp);
             if(diff < 0) diff = 0;
-            reward.xp = diff;
+            xpGain = diff;
         }
-        if(reward.xp > 0) await skillsModel.addXp(skillRecord.id, reward.xp);
+        if(xpGain > 0) await skillsModel.addXp(skillRecord.id, xpGain);
         await inventoryModel.add(data.user.id, reward.id, 1);
 
         await questsHelper.check('chop', args[0], 1, data.user, msg);
 
         const item = await itemsModel.get(reward.id);
-        return msg.channel.send(`**${data.user.name}** chopped a tree and got a ${item.name} ${reward.xp > 0 ? `and ${reward.xp}xp!` : ''}`);
+        return msg.channel.send(`**${data.user.name}** chopped a tree and got a ${item.name} ${xpGain > 0 ? `and ${xpGain}xp!` : ''}`);
     },
 };
