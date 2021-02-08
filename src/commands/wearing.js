7d8cf28bb846b8016e6e6343089f48b57921297e
Randy <phpgeweld@gmail.com>
2021-02-08 13:34:47 +0100

    more info about bonusses in wearing command

--- src/commands/wearing.js
+++ src/commands/wearing.js
@@ -19,6 +19,43 @@ module.exports = {
                 });
             }
         }
+        const attackBonus = {
+            stab: 0,
+            slash: 0,
+            crush: 0,
+            magic: 0,
+            ranged: 0,
+        };
+        const defenceBonus = {
+            stab: 0,
+            slash: 0,
+            crush: 0,
+            magic: 0,
+            ranged: 0,
+        };
+        const otherBonusses = {
+            melee: 0,
+            ranged: 0,
+            magic: 0,
+        };
+
+        fields.push({
+            name: 'Attack bonus',
+            inline: true,
+            value: `Stab: ${attackBonus.stab}\nSlash: ${attackBonus.slash}\nCrush: ${attackBonus.crush}\nMagic: ${attackBonus.magic}\nRange: ${attackBonus.ranged}`,
+        });
+
+        fields.push({
+            name: 'Defence bonus',
+            inline: true,
+            value: `Stab: ${defenceBonus.stab}\nSlash: ${defenceBonus.slash}\nCrush: ${defenceBonus.crush}\nMagic: ${defenceBonus.magic}\nRange: ${defenceBonus.ranged}`,
+        });
+
+        fields.push({
+            name: 'Other bonusses',
+            inline: true,
+            value: `Melee: ${otherBonusses.melee}\nRanged: ${otherBonusses.ranged}\nMagic: ${otherBonusses.magic}`,
+        });
 
         const embed = {
             title: `${data.user.name} wearing`,
