cb65841e80cd390597feb027d162bfe705d01e28
Randy <phpgeweld@gmail.com>
2021-02-08 11:52:09 +0100

    Skills command now works with new classes

--- src/commands/skills.js
+++ src/commands/skills.js
@@ -1,23 +1,16 @@
-const skillsModel = require('../models/usersSkills');
 const valuesHelper = require('../helpers/values');
-
-const skillsHelper = require('../helpers/skills');
 
 module.exports = {
     async run(msg, args, data) {
-        const userSkills = await skillsModel.getAllFor(data.user.id);
-
         const fields = [];
-        for (let i in userSkills) {
-            const level = skillsHelper.levelForXp(userSkills[i].xp);
-            const totalForNextLevel = skillsHelper.xpForLevel(level + 1);
-            const totalXp = parseInt(userSkills[i].xp);
-            const remainder = totalForNextLevel - totalXp;
+        const skills = data.char.skills;
 
+        for (let s in skills) {
             fields.push({
-                name: `${valuesHelper.ucfirst(userSkills[i].skill)} (${level})`,
-                value: `XP: ${userSkills[i].xp}\nTill next level: ${remainder}`,
-                //value: `Level: ${level}\nXP: ${userSkills[i].xp}\nNext level: ${totalForNextLevel}\nRemainder: ${remainder}`,
+                name: `${valuesHelper.ucfirst(skills[s].name)} (${
+                    skills[s].level
+                })`,
+                value: `XP: ${skills[s].xp}\nTill next level: ${skills[s].remainder}`,
                 inline: true,
             });
         }
