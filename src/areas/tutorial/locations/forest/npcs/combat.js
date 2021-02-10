4e5d12a94bc9eec79509359c31b562c7bf3c68e6
Randy <phpgeweld@gmail.com>
2021-02-10 13:27:42 +0100

    new quest

--- src/areas/tutorial/locations/forest/npcs/combat.js
+++ src/areas/tutorial/locations/forest/npcs/combat.js
@@ -1,7 +1,13 @@
+const questsModel = require('../../../../../models/usersQuests');
+
 module.exports = {
     label: 'combat instructor',
 
-    talk() {
-        return 'Hi';
+    async talk(data) {
+        const firstQuest = await questsModel.getFor(data.userID, 9);
+        if (!firstQuest) {
+            await questsModel.create(data.userID, 9);
+            return `Small explanation about combat\n.....\n\nNew quest: Kill a rat. \nMake sure you wield your dagger before you start!`;
+        }
     },
 };
