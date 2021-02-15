e3a7898d59f76093d2e2750487c6c4a97dddff52
Randy <phpgeweld@gmail.com>
2021-02-15 10:46:03 +0100

    Added woman to chicken farm

--- src/areas/lumbridgenorth/locations/chickenfarm/chickenfarm.js
+++ src/areas/lumbridgenorth/locations/chickenfarm/chickenfarm.js
@@ -93,5 +93,48 @@ module.exports = {
                 },
             },
         },
+        woman: {
+            amount: 3,
+            respawnTicks: 10,
+            xp: 8,
+            attackType: 'crush',
+            stats: {
+                combat: {
+                    hitpoints: 7,
+                    attack: 1,
+                    strength: 1,
+                    defence: 1,
+                    magic: 1,
+                    ranged: 1,
+                },
+                aggressive: {
+                    attack: 0,
+                    strength: 0,
+                    magic: 0,
+                    magicDamage: 0,
+                    ranged: 0,
+                    rangedStrength: 0,
+                },
+                defence: {
+                    stab: -21,
+                    slash: -21,
+                    crush: -21,
+                    magic: -21,
+                    ranged: -21,
+                },
+                immunities: { poison: false, venom: false },
+            },
+            loot: {
+                gold: {
+                    min: 3,
+                    max: 25,
+                },
+                53: {
+                    min: 1,
+                    max: 1,
+                    chance: 100,
+                },
+            },
+        },
     },
 };
