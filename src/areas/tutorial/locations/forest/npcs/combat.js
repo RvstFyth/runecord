48bf4a4b24c07e763687b7a1af65ecc0b6ba206c
Randy <phpgeweld@gmail.com>
2021-02-15 14:53:38 +0100

    Combat trainer text

--- src/areas/tutorial/locations/forest/npcs/combat.js
+++ src/areas/tutorial/locations/forest/npcs/combat.js
@@ -7,7 +7,18 @@ module.exports = {
         const firstQuest = await questsModel.getFor(data.userID, 9);
         if (!firstQuest) {
             await questsModel.create(data.userID, 9);
-            return `Small explanation about combat\n.....\n\nNew quest: Kill a rat. \nMake sure you wield your dagger before you start!`;
+            return (
+                `Hi there!\n` +
+                `I will teach you the basics about combat in RuneCord.\n\n` +
+                `The \`${data.prefix}loc\` command has a mobs field if there are mobs on your location. \n` +
+                `Use the \`${data.prefix}kill [mob]\` command to start attacking them. Once the fight is done, the loot will drop on the floor ` +
+                `(except for gold), so always use the \`${data.prefix}loc\` command to check for valuable loot. You can pickup items with the \`${data.prefix}pu [item]\` command. ` +
+                `As you progress mobs become stronger. So it's always good to wear the strongest armour you can get!` +
+                `\n\n**Combat styles**\nEach weapon has a few combat style, you can check this with the \`,styles\` command. This will ` +
+                `output the styles you can select and the style that is active. Each combat style trains a different skill.` +
+                `\n\nNow for your next quest, i want you to kill a rat.` +
+                `\nBefore you start, make sure you are wielding your dagger \`${data.prefix}wield bronze dagger\`!`
+            );
         }
         if (firstQuest && !firstQuest.completed)
             return `What are you waiting for? Just kill a rat already..`;
