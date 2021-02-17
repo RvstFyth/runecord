668363f982875fb8eb15946aeb7ab3e8d758aa57
Randy <phpgeweld@gmail.com>
2021-02-17 08:44:56 +0100

    logs spawns at lumbridge castle

--- src/areas/lumbridgecastle/locations/third/third.js
+++ src/areas/lumbridgecastle/locations/third/third.js
@@ -1,5 +1,11 @@
 module.exports = {
     label: 'Second floor',
     tools: ['bank'],
+    itemSpawns: {
+        1: {
+            amount: 4,
+            label: 'knife',
+        },
+    },
     description: `Dit is een faketekst. Alles wat hier staat is slechts om een indruk te geven van het grafische effect van tekst op deze plek. Wat u hier leest is een voorbeeldtekst. Deze wordt later vervangen door de uiteindelijke tekst, die nu nog niet bekend is. De faketekst is dus een tekst die eigenlijk nergens over gaat. Het grappige is, dat mensen deze toch vaak lezen. Zelfs als men weet dat het om een faketekst gaat, lezen ze toch door.`,
 };
