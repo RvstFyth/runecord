462df99dc561e293542d3c396db46e1468bc45d9
Randy <phpgeweld@gmail.com>
2021-02-17 18:34:50 +0100

    lummy shop completed

--- src/areas/lumbridgesquare/locations/generalgoods/npcs/storekeeper.js
+++ src/areas/lumbridgesquare/locations/generalgoods/npcs/storekeeper.js
@@ -11,5 +11,12 @@ module.exports = {
         { id: 66, price: 1 },
         { id: 68, price: 1 },
         { id: 62, price: 7 },
+        { id: 70, price: 2 },
+        { id: 71, price: 5 },
+        { id: 72, price: 13 },
+        { id: 38, price: 1 },
+        { id: 73, price: 1 },
+        { id: 74, price: 3 },
+        { id: 75, price: 1 },
     ],
 };
