5953f9d38ddfbae338337989c59261e691e367e4
Randy <phpgeweld@gmail.com>
2021-02-17 09:04:13 +0100

    more lummy item spawns

--- src/areas/lumbridgecastle/locations/first/first.js
+++ src/areas/lumbridgecastle/locations/first/first.js
@@ -1,4 +1,14 @@
 module.exports = {
     label: 'Ground floor',
+    itemSpawns: {
+        65: {
+            amount: 1,
+            label: 'pot',
+        },
+        66: {
+            amount: 1,
+            label: 'jug',
+        },
+    },
     description: `Dit is een faketekst. Alles wat hier staat is slechts om een indruk te geven van het grafische effect van tekst op deze plek. Wat u hier leest is een voorbeeldtekst. Deze wordt later vervangen door de uiteindelijke tekst, die nu nog niet bekend is. De faketekst is dus een tekst die eigenlijk nergens over gaat. Het grappige is, dat mensen deze toch vaak lezen. Zelfs als men weet dat het om een faketekst gaat, lezen ze toch door.`,
 };
--- src/areas/lumbridgecastle/locations/third/third.js
+++ src/areas/lumbridgecastle/locations/third/third.js
@@ -4,7 +4,7 @@ module.exports = {
     itemSpawns: {
         1: {
             amount: 4,
-            label: 'knife',
+            label: 'logs',
         },
     },
     description: `Dit is een faketekst. Alles wat hier staat is slechts om een indruk te geven van het grafische effect van tekst op deze plek. Wat u hier leest is een voorbeeldtekst. Deze wordt later vervangen door de uiteindelijke tekst, die nu nog niet bekend is. De faketekst is dus een tekst die eigenlijk nergens over gaat. Het grappige is, dat mensen deze toch vaak lezen. Zelfs als men weet dat het om een faketekst gaat, lezen ze toch door.`,
