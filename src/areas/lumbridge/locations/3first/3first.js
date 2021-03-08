b977a960823637b35baa49f0e73a4546704bfbcb
Randy <phpgeweld@gmail.com>
2021-03-08 21:25:44 +0100

    reorganized areas

--- src/areas/lumbridge/locations/3first/3first.js
+++ src/areas/lumbridge/locations/3first/3first.js
@@ -1,10 +1,13 @@
 module.exports = {
-    label: 'Second floor',
-    tools: ['bank'],
+    label: 'Castle Ground floor',
     itemSpawns: {
-        1: {
-            amount: 4,
-            label: 'logs',
+        65: {
+            amount: 1,
+            label: 'pot',
+        },
+        66: {
+            amount: 1,
+            label: 'jug',
         },
     },
     description: `Dit is een faketekst. Alles wat hier staat is slechts om een indruk te geven van het grafische effect van tekst op deze plek. Wat u hier leest is een voorbeeldtekst. Deze wordt later vervangen door de uiteindelijke tekst, die nu nog niet bekend is. De faketekst is dus een tekst die eigenlijk nergens over gaat. Het grappige is, dat mensen deze toch vaak lezen. Zelfs als men weet dat het om een faketekst gaat, lezen ze toch door.`,
