5d9d1e68f9117eb5dbec0b035652f4087494c975
Randy <phpgeweld@gmail.com>
2021-03-14 15:49:29 +0100

    compact overview for skills command

--- src/commands/skills.js
+++ src/commands/skills.js
@@ -5,17 +5,35 @@ module.exports = {
     async run(msg, args, data) {
         const fields = [];
         const skills = data.char.skills;
-
+        const compact = args.filter((a) => a === '-c').length > 0;
+        let compactField = { name: `\u200b`, value: '', inline: true };
+        let compactCounter = 0;
         for (let s in skills) {
             const em = await emojisHelper.get(msg.client, skills[s].name);
-            fields.push({
-                name: `${em} ${valuesHelper.ucfirst(skills[s].name)} (${
-                    skills[s].level
-                })`,
-                value: `XP: ${skills[s].xp}\nTill next level: ${skills[s].remainder}`,
-                inline: true,
-            });
+            if (compact) {
+                compactField.value += `${em} ${skills[s].level} | TNL: ${skills[s].remainder}\n`;
+                compactCounter++;
+
+                if (
+                    compactCounter % 7 === 0 ||
+                    compactCounter === Object.keys(skills).lengt - 1
+                ) {
+                    fields.push(compactField);
+                    compactField = { ...compactField };
+                    compactField.value = '';
+                }
+            } else {
+                fields.push({
+                    name: `${em} ${valuesHelper.ucfirst(skills[s].name)} (${
+                        skills[s].level
+                    })`,
+                    value: `XP: ${skills[s].xp}\nTill next level: ${skills[s].remainder}`,
+                    inline: true,
+                });
+            }
         }
+
+        if (compact) fields.push(compactField);
 
         let total = valuesHelper.sumArray(
             Object.values(skills).map((s) => s.level)
