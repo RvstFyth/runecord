d676fc1aa76f675cbd6de5c6bca27bca1e5ae4f8
Randy <phpgeweld@gmail.com>
2021-03-18 13:07:06 +0100

    updatd donate command with crypto optoins

--- src/commands/donate.js
+++ src/commands/donate.js
@@ -4,7 +4,12 @@ module.exports = {
             description:
                 `You can support the development of this bot with [Donatebot.io](https://donatebot.io/checkout/803223425258618901) which uses paypal. \n` +
                 `You can choose for a one-time payment or a monthly subscription. Besides getting a fancy role on our support server, you will also get 1 RCC for each USD donated\n` +
-                `You can use RCC for buying membership or XP lamps.`,
+                `You can use RCC for buying membership or XP lamps.\n\n` +
+                `**Donate with crypto**:\n` +
+                `After sending a transaction, ask a staff member on our support server to verify and grant rewards. If you want to donate with another coin, contact a developer on our support server.\n\n` +
+                `BTC: \`1PTsd6AB1kRbstvbAfHBLjXQp2jGAmx6xt\`\n` +
+                `ETH: \`0xb6bed45F7edd50D4aDCFD8839fdA57C41AC2b3Cb\`\n` +
+                `UBQ: \`0xdcFd4CBA0A1580F3D76741e4c9449f170adb60cE\`\n`,
         };
 
         return msg.channel.send({ embed });
