c1d3a647ed6d55887e9d6053ab3083a352ba3ba7
RvStFyth <phpgeweld@gmail.com>
2021-05-23 13:35:13 +0200

    formatted to code standards pickpocket command

--- src/commands/pickpocket.js
+++ src/commands/pickpocket.js
@@ -9,15 +9,32 @@ module.exports = {
     async run(msg, args, data) {
         const input = args.join(' ');
         if (!input) {
-            return msg.channel.send(`**${data.user.name}** who are you trying to pickpocket?!?`);
+            return msg.channel.send(
+                `**${data.user.name}** who are you trying to pickpocket?!?`
+            );
         }
 
-        const mobsAtLocation = worldsHelper.getMobs(data.user.world, data.user.area, data.user.location);
-        if (!Object.keys(mobsAtLocation).length) return msg.channel.send(`**${data.char.name}** there are no mobs to pickpocket here...`);
-        const filteredMobs = Object.values(mobsAtLocation[input]).filter((m) => m.mob.health > 0 && !m.occupied);
+        const mobsAtLocation = worldsHelper.getMobs(
+            data.user.world,
+            data.user.area,
+            data.user.location
+        );
+        if (!Object.keys(mobsAtLocation).length)
+            return msg.channel.send(
+                `**${data.char.name}** there are no mobs to pickpocket here...`
+            );
+        const filteredMobs = Object.values(mobsAtLocation[input]).filter(
+            (m) => m.mob.health > 0 && !m.occupied
+        );
 
-        if(!mobsAtLocation[input]) return msg.channel.send(`**${data.user.name}** there is no ${input} here...`);
-        if(!mobsAtLocation[input] || !filteredMobs.length) return msg.channel.send(`**${data.user.name}** there is no ${input} here...`);
+        if (!mobsAtLocation[input])
+            return msg.channel.send(
+                `**${data.user.name}** there is no ${input} here...`
+            );
+        if (!mobsAtLocation[input] || !filteredMobs.length)
+            return msg.channel.send(
+                `**${data.user.name}** there is no ${input} here...`
+            );
 
         const locationData = await areasHelper.getLocation(
             data.user.area,
@@ -25,35 +42,52 @@ module.exports = {
         );
 
         const mob = locationData.mobs[input];
-        if(!mob.pickpocket) return msg.channel.send(`**${data.user.name}** you can't pickpocket ${input}...`);
+        if (!mob.pickpocket)
+            return msg.channel.send(
+                `**${data.user.name}** you can't pickpocket ${input}...`
+            );
 
-        if (data.char.skills.thieving.level < mob.pickpocket.level) return msg.channel.send(`**${data.user.name}** you need thieving level ${mob.pickpocket.level} for this...`);
+        if (data.char.skills.thieving.level < mob.pickpocket.level)
+            return msg.channel.send(
+                `**${data.user.name}** you need thieving level ${mob.pickpocket.level} for this...`
+            );
 
         const seconds = random.number(15, 30);
 
         if (random.number(1, 100) <= mob.pickpocket.baseChance) {
-            const xpAdded = await data.char.skills.thieving.addXp(mob.pickpocket.xp);
+            const xpAdded = await data.char.skills.thieving.addXp(
+                mob.pickpocket.xp
+            );
             const loot = [];
             if (mob.pickpocket.loot.gold) {
-                const gold = random.number(mob.pickpocket.loot.gold.min, mob.pickpocket.loot.gold.max);
-                loot.push({name: 'gold', amount: gold});
+                const gold = random.number(
+                    mob.pickpocket.loot.gold.min,
+                    mob.pickpocket.loot.gold.max
+                );
+                loot.push({ name: 'gold', amount: gold });
                 await usersModel.addGold(data.char.id, gold);
             }
 
             const lootField = { name: 'Loot', value: '', inline: true };
             for (let i in loot) {
-                lootField.value += `${loot[i].amount} x ${loot[i].name}\n`
+                lootField.value += `${loot[i].amount} x ${loot[i].name}\n`;
             }
 
             const em = await emojisHelper.get(msg.client, 'thieving');
-            const xpField = { name: 'XP', value: `${xpAdded} ${em}`, inline: true };
+            const xpField = {
+                name: 'XP',
+                value: `${xpAdded} ${em}`,
+                inline: true,
+            };
 
             const embed = {
                 description: `**${data.user.name}** successfully pickpocketed ${input}`,
-                fields: [lootField, xpField]
-            }
-            return msg.channel.send({embed});
-        }
-        else return msg.channel.send(`**${data.user.name}** you failed to pickpocket ${input}..`);
-    }
+                fields: [lootField, xpField],
+            };
+            return msg.channel.send({ embed });
+        } else
+            return msg.channel.send(
+                `**${data.user.name}** you failed to pickpocket ${input}..`
+            );
+    },
 };
